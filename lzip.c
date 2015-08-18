fbf05a54437bdc7f5869f479bf3dcd07dcccdc98
Alex Sandro <geniofuturo@gmail.com>
2015-08-18 19:29:57 -0300

    Adding zip opening flags.

--- lzip.c
+++ lzip.c
@@ -1,7 +1,6 @@
 #include <lauxlib.h>
 #include <errno.h>
 #include "zip.h"
-#include "lua.h"
 #include "lzip.h"
 
 #define BUFSIZE 1024 * 2
@@ -11,8 +10,12 @@
 static void lzip_open(lua_State *L) {
     const char *path = luaL_check_string(L, 1); // file path
 
-    int flags = 0; // zip flags
-    int zep = 0; // open status
+     // zip flags
+    int flags = (int) lua_getnumber(L, lua_getparam(L, 2));
+    flags = flags  == 0 ? ZIP_CHECKCONS : flags;
+
+    // open status
+    int zep = 0;
 
     // open the file
     struct zip * zip_s;
@@ -54,7 +57,38 @@ static struct luaL_reg lzip[] = {
 };
 
 
+#define set_table(L, obj, name, value) \
+    lua_pushobject(L, obj); \
+    lua_pushstring(L, name); \
+    lua_pushnumber(L, value); \
+    lua_settable(L);
+
+
 int LUA_LIBRARY lua_lzipopen(lua_State *L) {
     luaL_openlib(L, lzip, (sizeof(lzip)/sizeof(lzip[0])));
+
+    // zip open modes
+    lua_Object modes = lua_createtable(L);
+
+    // create the global table flags
+    lua_pushobject(L, modes);
+    lua_setglobal(L, "zip_open_flags");
+
+    // ZIP_CHECKCONS: Perform additional stricter consistency checks on the archive, and error if they fail.
+    set_table(L, modes, "ZIP_CHECKCONS", ZIP_CHECKCONS);
+
+    // ZIP_CREATE: Create the archive if it does not exist.
+    set_table(L, modes, "ZIP_CREATE", ZIP_CREATE);
+
+    // ZIP_EXCL: Error if archive already exists.
+    set_table(L, modes, "ZIP_EXCL", ZIP_EXCL);
+
+    // ZIP_TRUNCATE: If archive exists, ignore its current contents. In other words, handle it the same way as an empty archive.
+    set_table(L, modes, "ZIP_TRUNCATE", ZIP_TRUNCATE);
+
+    // ZIP_RDONLY: Open archive in read-only mode.
+    // set_table(L, modes, "ZIP_RDONLY:", ZIP_RDONLY);
+
+    lua_pushobject(L, modes); // end table
     return 0;
 }
